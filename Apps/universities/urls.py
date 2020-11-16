fd73f252dbc7ec48b30beb9056e3f053cec57336
ebubekir <ebubekirustalar@gmail.com>
2020-11-16 19:44:56 +0300

    universities edited

--- Apps/universities/urls.py
+++ Apps/universities/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'universities'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detail/<int:university_id>', views.detail, name='detail'),
+    path('createUniversity/', views.createUniversity, name='createUniversity'),
 ]
--- Apps/universities/views.py
+++ Apps/universities/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import University
 
 # Create your views here.
 
 def index(request):
     return render(request, 'universities/index.html', {})
+
+def detail(request, university_id):
+    post = get_object_or_404(University, pk=university_id)
+    return render(request, 'universities/detail.html', {})
+
+def createUniversity(request):
+    if request.user.is_superuser:
+        return render(request, 'universities/createUniversity.html', {})
+    else:
+        return render(request, 'universities/index.html', {})
