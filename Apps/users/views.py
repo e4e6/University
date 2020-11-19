ed507c7d8b68efc7344af423fc0ad47c2bd134da
e4e6 <73366454+e4e6@users.noreply.github.com>
2020-11-19 19:02:35 +0300

    deneme1

--- Apps/users/views.py
+++ Apps/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render ,redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import authenticate, logout as logoutUser, login as loginUser
 from django.utils import timezone
 from django.views.generic.base import View
@@ -9,14 +9,14 @@ from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
 from django.contrib import messages
 from django.core.mail import EmailMessage
-from django.utils.encoding import force_bytes,force_text,DjangoUnicodeDecodeError
+from django.utils.encoding import force_bytes, force_text, DjangoUnicodeDecodeError
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
 from .utils import token_generator
 from django.conf import settings
 
-User=get_user_model()
+User = get_user_model()
 
 
 # NECESSARY FUNCIONS FOR VIEWS
@@ -24,83 +24,74 @@ User=get_user_model()
 def index(request):
     return render(request, 'users/index.html', {})
 
+
 def universityList(request):
     return render(request, 'users/universityList.html', {})
+
 
 def eventList(request):
     return render(request, 'users/eventList.html', {})
 
+
 def profile(request):
     return render(request, 'users/profile.html', {})
 
+
 def profileEdit(request):
     return render(request, 'users/profileEdit.html', {})
-
-
-# def password_changeaa(request):
-#     return render(request, 'users/password_change.html', {})
-
 
 
 # NECESSARY FUNCIONS
 
 def login(request):
     if request.method == 'POST':
-        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        user = authenticate(
+            username=request.POST['username'], password=request.POST['password'])
         if user is not None:
             # A backend authenticated the credentials
             loginUser(request, user)
             return render(request, 'users/index.html', {'alert_message': 'Başarılı', 'alertColor': 'successfull'})
         else:
-            # No backend authenticated the credentials    
+            # No backend authenticated the credentials
             return render(request, 'users/index.html', {'alert_message': 'Kullanıcı girişi yapılamadı Lütfen Tekrar deneyin', 'alertColor': 'dangerr'})
+
 
 def register(request):
     if request.method == 'POST':
-        #try:
-            if request.POST['password1'] == request.POST['password2']:
-                username = request.POST['username']
-                email = request.POST['email']
-                password = request.POST['password1']
-                user = CustomUser.objects.create_user(username, email, password)
-                user.is_active=False
-               
-                
-                user.save()
-
-                uidb64=urlsafe_base64_encode(force_bytes(user.pk))
-
-                domain=get_current_site(request).domain
-                link=reverse('users:activate',kwargs={'uidb64':uidb64,'token':token_generator.make_token(user)})
-                activate_url='http://'+domain+link
-                email_subject="hesabınızı aktifleştirin"
-                email_body= user.username + 'linke tıklayınız\n' + activate_url
-                emailsend=EmailMessage(
-                    email_subject,
-                    email_body,
-                    'portaluniversite@gmail.com',
-                    [user.email]
-
-
-
-
-
-
-                )
-                emailsend.send(fail_silently=False)
-
-                return render(request, 'users/index.html', {'alert_message': 'Başarılı', 'alertColor': 'successfull'})
-            else:
-                return render(request, 'users/index.html', {'alert_message': 'Şifreler Eşleşmedi', 'alertColor': 'dangerr'})
-        #except:
-            #return render(request, 'users/index.html', {'alert_message': 'Bir sorun ile karşılaşıldı', 'alertColor': 'danger'})
+        # try:
+        if request.POST['password1'] == request.POST['password2']:
+            username = request.POST['username']
+            email = request.POST['email']
+            password = request.POST['password1']
+            user = CustomUser.objects.create_user(username, email, password)
+            user.is_active = False
+            user.save()
+            uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+            domain = get_current_site(request).domain
+            link = reverse('users:activate', kwargs={
+                           'uidb64': uidb64, 'token': token_generator.make_token(user)})
+            activate_url = 'http://'+domain+link
+            email_subject = "hesabınızı aktifleştirin"
+            email_body = user.username + '   linke tıklayınız\n' + activate_url
+            emailsend = EmailMessage(
+                email_subject,
+                email_body,
+                'portaluniversite@gmail.com',
+                [user.email]
+            )
+            emailsend.send(fail_silently=False)
+            return render(request, 'users/index.html', {'alert_message': 'Başarılı', 'alertColor': 'successfull'})
+        else:
+            return render(request, 'users/index.html', {'alert_message': 'Şifreler Eşleşmedi', 'alertColor': 'dangerr'})
+        # except:
+        # return render(request, 'users/index.html', {'alert_message': 'Bir sorun ile karşılaşıldı', 'alertColor': 'danger'})
     else:
         return render(request, 'users/index.html', {'alert_message': 'Bir sorun ile karşılaşıldı', 'alertColor': 'dangerr'})
+
 
 def logout(request):
     logoutUser(request)
     return render(request, 'users/index.html', {'alert_message': 'Başarılı', 'alertColor': 'successfull'})
-    
 
 
 def password_change(request):
@@ -109,8 +100,9 @@ def password_change(request):
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
-            messages.success(request, 'Your password was successfully updated!')
-            return redirect('password_change')
+            messages.success(
+                request, 'Your password was successfully updated!')
+            return redirect('users:password_change')
         else:
             messages.error(request, 'Please correct the error below.')
     else:
@@ -121,9 +113,9 @@ def password_change(request):
 
 
 class VerificationView(View):
-    def get(self,request, uidb64 , token):
+    def get(self, request, uidb64, token):
         try:
-            
+
             uid = force_text(urlsafe_base64_decode(uidb64))
             user = User.objects.get(pk=uid)
         except (TypeError, ValueError, OverflowError, User.DoesNotExist):
@@ -134,8 +126,8 @@ class VerificationView(View):
             user.save()
             #login(request, user)
             messages.success(request, ('hesabınız oluşturuldu'))
-            return redirect ('users:index')
-        else:
-            messages.warning(request, ('doğrulama linki geçersiz, bir ihtimal daha önce kullanıldı'))
             return redirect('users:index')
-
+        else:
+            messages.warning(
+                request, ('doğrulama linki geçersiz, bir ihtimal daha önce kullanıldı'))
+            return redirect('users:index')
